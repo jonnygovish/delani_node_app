75fb8ce1adacf538b238f747832d229f94e3dc30
john mutavi <jonnygovish@gmail.com>
2020-05-09 12:57:52 +0300

    turning server.js into a module.

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 let http = require('http');
+// A function to wrap our server functionality so that we can export it
+let start = function(){
 
-function onRequest(request, response){
-    console.log("Request received!")
-	response.writeHead(200, {"Content-type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+	function onRequest(request, response){
+		console.log("Request received!")
+		response.writeHead(200, {"Content-type": "text/plain"});
+		response.write("Hello World");
+		response.end();
+	}
+	
+	http.createServer(onRequest).listen(8000);
+	
+	console.log("Server has started.")
 }
 
-http.createServer(onRequest).listen(8000);
-
-console.log("Server has started.")
+exports.start = start;
