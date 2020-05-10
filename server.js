d33c55eacef5d01c0aa985663c4e654ecfce7a49
john mutavi <jonnygovish@gmail.com>
2020-05-10 16:26:05 +0300

    Extract pathname from the request URL

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 let http = require('http');
+let url = require('url');
+
 // A function to wrap our server functionality so that we can export it
 let start = function(){
 
 	function onRequest(request, response){
+		// Extracting the pathname from the url requested
+		let pathname = url.parse(request.url).pathname
+		console.log(pathname)
 		console.log("Request received!")
 		response.writeHead(200, {"Content-type": "text/plain"});
 		response.write("Hello World");
