6db06240a033db6eca993e8dd96b7e19b6c45fea
john mutavi <jonnygovish@gmail.com>
2020-05-11 13:15:31 +0300

    Create an object and map different urls to the request handlers functions

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 let server = require('./server');
 let router = require('./router');
+let requestHandlers = require("./requestHandler");
 
-server.start(router.route);
+let handle = {};
+handle["/"] = requestHandlers.index;
+handle["/index.html"] = requestHandlers.index;
+handle["/portfolio.html"] = requestHandlers.portfolio;
+
+server.start(router.route, handle);
