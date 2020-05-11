15a1cf92cce393a51c56f4ff8ee1c3b06e6960cb
john mutavi <jonnygovish@gmail.com>
2020-05-11 13:14:33 +0300

    add the handle object from index.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let http = require('http');
 let url = require('url');
 
 // A function to wrap our server functionality so that we can export it
-let start = function(route){
+let start = function(route, handle){
 
 	function onRequest(request, response){
 		// Extracting the pathname from the url requested
@@ -10,7 +10,7 @@ let start = function(route){
 		console.log("Request for " + pathname + " has been received.")
 		
 		// Passing the pathname as a parameter to the route function
-		route(pathname);
+		route(handle, pathname);
 
 		response.writeHead(200, {"Content-type": "text/plain"});
 		response.write("Hello World");
