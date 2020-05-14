0c6eec4f62d644bad73b0f09c31d0fe24dc66608
john mutavi <jonnygovish@gmail.com>
2020-05-14 12:14:22 +0300

    Serving content to the browser

--- requestHandler.js
+++ requestHandler.js
@@ -1,19 +1,26 @@
+let exec = require("child_process").exec;
+
 // function for handling the business logic for index.html
-function index (){
+function index (response){
     console.log("Request handler for index was called.")
-    // Function that will delay for 10 seconds before returning the text.
-    function sleep(miliseconds){
-        var startTime = new Date().getTime()
-        while (new Date().getTime() < startTime + miliseconds){}
-    };
-    sleep(10000);
-    return "Welcome to Delani Studio"
+    
+    exec("ls -lah", function(error, stdout, stderr){
+        
+        let content = stdout;
+        response.writeHead(200, {"Content-type": "text/plain"});
+		response.write(content);
+		response.end();
+    })
+
 }
 
 // function for handling the business logic for  portfolio.html
-function portfolio(){
+function portfolio(response){
     console.log("Request for handler for portfolio was called.")
-    return "These are some of our portfolio projects"
+    response.writeHead(200, {"Content-type": "text/plain"});
+    response.write("These are some of our portfolio projects");
+    response.end();
+    
 }
 
 
