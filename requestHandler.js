88a217f0637491b4d19e66b2664a941bec95e6ff
john mutavi <jonnygovish@gmail.com>
2020-05-23 13:50:31 +0300

    Add logging system to request handler module

--- requestHandler.js
+++ requestHandler.js
@@ -1,12 +1,16 @@
 let fs = require("fs");
+let Logger = require('./logger');
+let logger = new Logger('Request Handler');
 
 // function for handling the business logic for index.html
 function index (response){
+    logger.info("Request handler for index was called.")
     console.log("Request handler for index was called.")
     
     // Reading the index.html file and attaching the content to the response
-    fs.readFile("./public/index.html",function(error, data){
-        if (error){
+    fs.readFile("./public/index.html",function(err, data){
+        if (err){
+            logger.error(err)
             console.log(err);  
         } 
         response.writeHead(200, {"Content-type": "text/html"});
@@ -17,6 +21,7 @@ function index (response){
 
 // function for handling the business logic for  portfolio.html
 function portfolio(response){
+    logger.info("Request for handler for portfolio was called.")
     console.log("Request for handler for portfolio was called.")
     response.writeHead(200, {"Content-type": "text/plain"});
     response.write("These are some of our portfolio projects");
